2507a0f638d68208c0e8bf6ed8c161e3a437ba22
Nezo0913 <156890722+Nezo0913@users.noreply.github.com>
2026-05-14 11:33:45 +0800

    updated firebase config

--- Backend/firebase.js
+++ Backend/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDdfVC4M-1MXilG4BQZFUlEEPYZFxXeJgU",
+  authDomain: "drawme-v2.firebaseapp.com",
+  databaseURL: "https://drawme-v2-default-rtdb.firebaseio.com",
+  projectId: "drawme-v2",
+  storageBucket: "drawme-v2.firebasestorage.app",
+  messagingSenderId: "1032891705090",
+  appId: "1:1032891705090:web:573d6a7bcca02f0b2b441b",
+  measurementId: "G-M6QVYEQNKH"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
